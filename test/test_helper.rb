b477f6581483dcfc8099f2662b60ad3e21bec7d5
Chap Ambrose <chapambrose@gmail.com>
2018-07-17 10:36:33 -0500

    ENV['GOOGLE_CHROME_BIN']

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
-Selenium::WebDriver::Chrome.driver_path = GOOGLE_CHROME_BIN
+Selenium::WebDriver::Chrome.driver_path = ENV['GOOGLE_CHROME_BIN']
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
