39089957ddec4a15bc2ae4c95cd0b3bd4c01de19
Chap Ambrose <chapambrose@gmail.com>
2018-07-17 12:04:42 -0500

    Update test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,26 +3,6 @@ require_relative '../config/environment'
 require 'rails/test_help'
 
 
-# Selenium::WebDriver::Chrome.driver_path = ENV['GOOGLE_CHROME_BIN']
-chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
-
-chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
-
-Capybara.register_driver :chrome_shim do |app|
-  Capybara::Selenium::Driver.new(
-     app,
-     browser: :chrome,
-     desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
-  )
-end
-
-Capybara.javascript_driver = :chrome_shim
-
-# options = Selenium::WebDriver::Firefox::Options.new
-# options.binary = "/path/to/firefox" 
-# driver = Selenium::WebDriver.for :firefox, options: options
-
-
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
