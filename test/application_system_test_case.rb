68f41cd291f82c8c93005f1504728eeb91bc6d56
Chap Ambrose <chapambrose@gmail.com>
2018-07-17 11:30:08 -0500

    back to just setting driver_path

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,14 +1,15 @@
 require "test_helper"
 
-chrome_bin = ENV.fetch('GOOGLE_CHROME_BIN', nil)
-chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
-Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(
-     app,
-     browser: :chrome,
-     desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
-  )
-end
+Selenium::WebDriver::Chrome.driver_path = ENV['GOOGLE_CHROME_BIN']
+# chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
+# chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
+# Capybara.register_driver :chrome do |app|
+#   Capybara::Selenium::Driver.new(
+#      app,
+#      browser: :chrome,
+#      desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
+#   )
+# end
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
