e65fe222b3f9a07969d20f6e4dc08f543e36634f
Chap Ambrose <chapambrose@gmail.com>
2018-07-17 12:56:12 -0500

    clean up

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,27 +1,18 @@
 require "test_helper"
 
 
-  
-  
-# Selenium::WebDriver::Chrome.driver_path = ENV['GOOGLE_CHROME_BIN']
+# tell Heroku CI run where to find chrome binary
 chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
-
 chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
-
-Capybara.register_driver :chrome_shim do |app|
+Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(
      app,
      browser: :chrome,
      desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
   )
 end
-
-Capybara.javascript_driver = :chrome_shim
+Capybara.javascript_driver = :chrome
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-# options = Selenium::WebDriver::Firefox::Options.new
-# options.binary = "/path/to/firefox" 
-# driver = Selenium::WebDriver.for :firefox, options: options
-
-  driven_by :chrome_shim
+  driven_by :chrome
 end
