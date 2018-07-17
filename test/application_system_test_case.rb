87007fc39073c17b18cd2027692f50a0872a7fee
Chap Ambrose <chapambrose@gmail.com>
2018-07-17 11:42:49 -0500

    try headless_chrome

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-Selenium::WebDriver::Chrome.driver_path = ENV['GOOGLE_CHROME_BIN']
+# Selenium::WebDriver::Chrome.driver_path = ENV['GOOGLE_CHROME_BIN']
 # chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
 # chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
 # Capybara.register_driver :chrome do |app|
@@ -11,6 +11,10 @@ Selenium::WebDriver::Chrome.driver_path = ENV['GOOGLE_CHROME_BIN']
 #   )
 # end
 
+# options = Selenium::WebDriver::Firefox::Options.new
+# options.binary = "/path/to/firefox" 
+# driver = Selenium::WebDriver.for :firefox, options: options
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
 end
